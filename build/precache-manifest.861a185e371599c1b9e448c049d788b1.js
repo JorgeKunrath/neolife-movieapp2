238cf743187229cba1483f7e88a93bba5f6bcacb
JorgeKunrath <54911848+JorgeKunrath@users.noreply.github.com>
2020-02-10 02:39:03 -0300

    add /build (produção)

---
+++ build/precache-manifest.861a185e371599c1b9e448c049d788b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde8d6cc352f163d44c48058c02fb630",
+    "url": "/neolife-movieapp2/index.html"
+  },
+  {
+    "revision": "dee69f59eed030c50a1d",
+    "url": "/neolife-movieapp2/static/css/main.a4628b42.chunk.css"
+  },
+  {
+    "revision": "363b97d068fef8a2b9f6",
+    "url": "/neolife-movieapp2/static/js/2.bf8ae319.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/neolife-movieapp2/static/js/2.bf8ae319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee69f59eed030c50a1d",
+    "url": "/neolife-movieapp2/static/js/main.4e7132a7.chunk.js"
+  },
+  {
+    "revision": "06bcc84d57e35f3a68b9",
+    "url": "/neolife-movieapp2/static/js/runtime-main.acc2089b.js"
+  }
+]);
